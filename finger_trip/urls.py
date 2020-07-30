0ce38dbb2211ab2a7f808fd9427227f70a295b0a
Apoorva-14 <apoorvasharma141195@gmail.com>
2020-07-31 00:00:25 +0530

    Model for the project

--- finger_trip/urls.py
+++ finger_trip/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from trip import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +30,6 @@ urlpatterns = [
 
     #uploading
     path('current/', views.currentuploads, name="currentuploads")
-
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
